3e683db4f2485bba95d565c71e06e35a749b10f7
dodo <sxnico@outlook.com>
2021-12-05 22:16:46 +0800

    12_5

--- pythonProject_11_25/test.py
+++ pythonProject_11_25/test.py
@@ -1,10 +1,19 @@
-number = input('shuzi')
-c = 1 if number == 2 else 2
-print(c)
-n = 0
-while n < 10:
-    if n % 3 == 0:
-        print(n)
-    else:
-        print(n+1)
-    n += 1
+# number = input('shuzi')
+# c = 1 if number == 2 else 2
+# print(c)
+# n = 0
+# while n < 10:
+#     if n % 3 == 0:
+#         print(n)
+#     else:
+#         print(n+1)
+#     n += 1
+# print('*'*20)
+# i = 0
+# while i < 10:
+#     if i == 10:
+#         break
+#     print(i)
+#     i += 1
+# else:
+#     print('down')
