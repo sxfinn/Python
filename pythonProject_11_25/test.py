83f4b31d6272541193995c960763e678d78c5004
dodo <sxnico@outlook.com>
2021-12-22 22:11:32 +0800

    12_22

--- pythonProject_11_25/test.py
+++ pythonProject_11_25/test.py
@@ -1,3 +1,37 @@
+def add(a, b):
+    return int(a) + int(b)
+
+
+a = 0
+b = 0
+a = (input('input_a'))
+b = (input('input_b'))
+print('ans = %d' % add(a, b))
+
+# def modify(a):
+#     i = 0
+#     while (i < 10):
+#         a[i] = a[i] + 1
+#         i += 1
+#
+#
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# modify(a)
+# print(a)
+
+# a = 'b'
+# print(type(a))
+
+
+# def fib(n):
+#     a, b = 1, 1
+#     while (a < n):
+#         print(a,end=' ')
+#         a, b = b, a + b
+#     print()
+#
+#
+# print(fib(4))
 
 # for i in range(0,10,2):
 #     print(i)
@@ -5,6 +39,7 @@
 # c = 1 if number == 2 else 2
 # print(c)
 # n = 0
+
 # while n < 10:
 #     if n % 3 == 0:
 #         print(n)
@@ -20,3 +55,4 @@
 #     i += 1
 # else:
 #     print('down')
+# print('hehe')
