76711e3935bccfcf8d15bbd8a852bb10715c6075
dodo <sxnico@outlook.com>
2021-12-26 23:22:08 +0800

    12_26

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -1,2 +1,183 @@
-for i in range(10):
-    print(i)
+def f2(a):
+    assert isinstance(a,int),'a must be a interger'
+    str_a=str(a)
+    return (int(str_a[::-1]))
+
+f2(123)
+
+
+# def f(a):
+#     if a == 1:
+#         print('123456 is %e' % 123456)
+#     elif a == 2:
+#         print('the second is{1:} and first is {0:},third is {2:}'.format(1, 2, 3))
+#     else:
+#         print(f'{a + 1} and {a + 2},{a + 3}')
+#
+#
+# f(30)
+
+# def f(a):
+#     if a == 1:
+#         print('123456 is %e' % 123456)
+#     elif a == 2:
+#         print('the second is{1:} and first is {0:},third is {2:}'.format(1, 2, 3))
+#     else:
+#         print(f'{a + 1} and {a + 2},{a + 3}')
+#
+#
+# f(3)
+#
+#
+# def f(a):
+#     if a == 1:
+#         print('123456 is %e' % 123456)
+#     elif a == 2:
+#         print('the second is{1:} and first is {0:},third is {2:}'.format(1, 2, 3))
+#     else:
+#         print(f'{a + 1} and {a + 2},{a + 3}')
+#
+#
+# f(1)
+#
+#
+# def f(a,b,c):
+#     return(a+b+c)
+#
+# d={'a':1,'b':2,'c':3}
+# print(f(**d))
+#
+#
+#
+# def f(a,b,c):
+#     return(a+b+c)
+#
+# d={1:'a',2:'b',3:'c'}
+# print(f(*d.values()))
+
+
+# def f(a):
+#     global x
+#     x = 3 + a
+#
+#
+# x = 6
+# f(4)
+# print(x)
+# def f(a):
+#     x = a + 1
+#
+#
+# x = 5
+# a = 6
+# f(a)
+# print(x)
+#
+# def f(a, b, c):
+#     print(c + b + a)
+#
+#
+# a = [1, 2, 3]
+# f(*a)
+#
+#
+# def f(a, b, c):
+#     print(c,b,a)
+#
+# a=[1,2,3]
+# f(*a)
+#
+# def f(*p):
+#     print(p)
+#
+#
+# f(1, 2)
+#
+#
+# def say1(message, times):
+#     print(message * times)
+#
+#
+# say1('hello', 3)
+# n = 1 + 1
+# print(n)
+# def f(x,y,z):
+#     print(z,y,x)
+#
+# f(1,2,3,4)
+
+# a=10
+# b='Hello World'
+# c=[1,2,3]
+# print(f'The first item is {b} and the second item is {c},the last item is {a+20}')
+#
+#
+# print( 'The second number is {1:} and the first number is {0:},the third number is {2:}' .format(0,3,10))
+# print('The number 112345 is %e'%112345)
+# a = 2
+#
+# print(--2)
+#
+# string = 'hello'
+# print(string[-1::-2])
+
+# a,b,c=1+2,2+3,3+4
+# a,b,c=a+b+c,c+3,a*3
+# d=[a,b,c]
+# print(d)
+
+# def f(x,y):
+#     z = x + y
+#
+# b = 0
+# b = f(3,9)
+# print(b)
+# a = list(range(4,2,-1))
+# print(a)
+# f = lambda a, b :a + b
+# print(f(3,4))
+# a = '中国成都AAA'
+# print(len(a))
+# print('abcd' is a)
+# a = [1,2,3,4]
+# print(2 is a)
+# print(3 is 3)
+# # print(10 > 20 and 1)
+# print(-2 < -1 < 0)
+# print('abc\kls')
+#
+# print(int(6.6)/3)
+# t = (123,'hello',123)
+# print(t[1][0])
+
+#
+#
+# for i in range(1,5):
+#     print(i)
+#     if i >= 3:
+#         continue
+
+# s = 1
+# s += 9
+# print(s)
+#
+# a = {1, 2, 3}
+# if a:
+#     print('set')
+# else:
+#     print('empty set')
+#
+# if 3:
+#     print(3)
+# else:
+#     print(4)
+
+# for i in range(10):
+#     print('a')
+# s1 = 'abc'
+# print(s1 * 3)
+#
+# s2 = 'efg'
+# print(s1 + s2)
+# for i in range(10):
+#     print(i)
